bdd9f9d7f9526e26a8e01eb9c24bb788cf4868b1
adam n <adam.nawroc@gmail.com>
2018-04-20 13:21:22 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -53,7 +53,7 @@ class SongsController < ApplicationController
     else
       # songs is collection of objs i assume
       @song = artist.songs.find_by(id: params[:id])
-      redirect_to artist_songs_path(artist), alert: "Post not found." if @song.nil?
+      redirect_to artist_songs_path(artist), alert: "Song not found." if @song.nil?
     end
   else
     @song = Song.find(params[:id])
